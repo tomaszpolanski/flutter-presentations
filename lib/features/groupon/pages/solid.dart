d8e03b50d90c8914b48f066b64707f9a66a48126
Tomek Polanski <tpolanski@groupon.com>
2019-03-08 15:35:14 -0300

    Making SOLID slide

--- lib/features/groupon/pages/solid.dart
+++ lib/features/groupon/pages/solid.dart
@@ -1,38 +1,179 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_presentations/shared/presentation_controller.dart';
+import 'package:flutter_presentations/shared/presentation_stepper.dart';
 
 class Solid extends StatefulWidget {
+  const Solid({Key key, this.controller}) : super(key: key);
+  final PresentationController controller;
+
   @override
   _SolidState createState() => _SolidState();
 }
 
-class _SolidState extends State<Solid> {
+const _principles = [
+  'Single Responsibility Principle',
+  'Open/Closed Principle',
+  'Liskov Substitution Principle',
+  'Interface Segregation Principle',
+  'Dependency Inversion',
+];
+
+enum _Step {
+  init,
+  solid,
+  next,
+}
+
+class _SolidState extends State<Solid> with TickerProviderStateMixin {
+  PageStepper<_Step> _stateController;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _stateController = PageStepper<_Step>(
+      controller: widget.controller,
+      steps: _Step.values,
+    )
+      ..add(
+        fromStep: _Step.init,
+        toStep: _Step.solid,
+        forward: () => _controller.forward(),
+        reverse: () => _controller.reverse(),
+      )
+      ..add(
+        fromStep: _Step.solid,
+        toStep: _Step.next,
+        forward: () => widget.controller.next(),
+      )
+      ..addListener(() => setState(() {}))
+      ..build();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _stateController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     const height = 140.0;
+    const offset = 130.0;
+    final translateAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0.0, 0.3, curve: Curves.ease),
+    );
+
     return DefaultTextStyle.merge(
       style: TextStyle(fontSize: height),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(
         children: [
-          _Letter('Single Responsibility Principle'),
-          Transform.translate(
-            offset: Offset(0, height * 1),
-            child: _Letter('Open/Closed Principle'),
+          _AnimatedLine(
+            index: 0,
+            animation: _controller,
           ),
-          Transform.translate(
-            offset: Offset(0, height * 2),
-            child: _Letter('Liskov Substitution Principle'),
+          Positioned(
+            left: offset,
+            child: AnimatedBuilder(
+              animation: translateAnimation,
+              builder: (_, child) {
+                return Transform.translate(
+                  offset: Offset(0, translateAnimation.value * height * 1),
+                  child: child,
+                );
+              },
+              child: _AnimatedLine(
+                index: 1,
+                animation: _controller,
+              ),
+            ),
           ),
-          Transform.translate(
-            offset: Offset(0, height * 3),
-            child: _Letter('Interface Segregation Principle'),
+          Positioned(
+            left: offset * 2,
+            child: AnimatedBuilder(
+              animation: translateAnimation,
+              builder: (_, child) {
+                return Transform.translate(
+                  offset: Offset(0, translateAnimation.value * height * 2),
+                  child: child,
+                );
+              },
+              child: _AnimatedLine(
+                index: 2,
+                animation: _controller,
+              ),
+            ),
           ),
-          Transform.translate(
-            offset: Offset(0, height * 4),
-            child: _Letter('Dependency Inversion'),
+          Positioned(
+            left: offset * 3,
+            child: AnimatedBuilder(
+              animation: translateAnimation,
+              builder: (_, child) {
+                return Transform.translate(
+                  offset: Offset(0, translateAnimation.value * height * 3),
+                  child: child,
+                );
+              },
+              child: _AnimatedLine(
+                index: 3,
+                animation: _controller,
+              ),
+            ),
+          ),
+          Positioned(
+            left: offset * 3.5,
+            child: AnimatedBuilder(
+              animation: translateAnimation,
+              builder: (_, child) {
+                return Transform.translate(
+                  offset: Offset(0, translateAnimation.value * height * 4),
+                  child: child,
+                );
+              },
+              child: _AnimatedLine(
+                index: 4,
+                animation: _controller,
+              ),
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _AnimatedLine extends StatelessWidget {
+  const _AnimatedLine({
+    Key key,
+    @required this.animation,
+    @required this.index,
+  }) : super(key: key);
+
+  final int index;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    const revealStart = 0.2;
+    final step = (1 - 0.2) / _principles.length.toDouble();
+    final start = revealStart + index * step;
+    return _Letter(
+      _principles[index],
+      animation: CurvedAnimation(
+        parent: animation,
+        curve: Interval(
+          start,
+          start + step,
+          curve: Curves.ease,
+        ),
       ),
     );
   }
@@ -48,15 +189,45 @@ class _Dot extends StatelessWidget {
 }
 
 class _Letter extends StatelessWidget {
-  const _Letter(this.text, {Key key}) : super(key: key);
+  const _Letter(
+    this.text, {
+    Key key,
+    this.animation,
+  }) : super(key: key);
   final String text;
+  final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
       children: <Widget>[
         Text(text[0]),
+        AnimatedBuilder(
+          animation: animation,
+          builder: (_, child) {
+            return Container(
+              foregroundDecoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white.withOpacity(0),
+                    Colors.white.withOpacity(0),
+                    Colors.white,
+                    Colors.white,
+                  ],
+                  stops: [0.0, animation.value, animation.value, 1.0],
+                ),
+              ),
+              child: child,
+            );
+          },
+          child: Text(
+            text.substring(1, text.length),
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
       ],
     );
   }
