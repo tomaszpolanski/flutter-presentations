4d02f8efe52a83ff7c4049af9402835adc91f6f9
Tomek Polanski <tpolanski@groupon.com>
2019-03-12 23:05:41 -0300

    Conversation

---
+++ lib/shared/animation_builder.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+
+class AnimationBuilder<T> extends StatelessWidget {
+  const AnimationBuilder({
+    Key key,
+    @required this.animation,
+    @required this.builder,
+    @required this.child,
+  }) : super(key: key);
+
+  final Animation<T> animation;
+  final Widget Function(
+    BuildContext context,
+    Animation<T> animation,
+    Widget child,
+  ) builder;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) => builder(context, animation, child),
+      child: child,
+    );
+  }
+}
