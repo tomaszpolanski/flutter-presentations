28783f20039c272a78a4dbcadafbdd4d3a5c9d19
Tomek Polanski <tpolanski@groupon.com>
2019-04-21 15:38:35 +0200

    Flutter driver tests

---
+++ test_driver/main_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Flutter Driver demo', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        await driver.close();
+      }
+    });
+
+    test('tap slivers', () async {
+      await driver.tap(find.text('Slivers!'));
+    });
+  });
+}
