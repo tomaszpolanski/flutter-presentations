973d31ea4169763dbdc2f24246a3d73cb55c1bc7
Tomasz Polański <polanski.tomek@gmail.com>
2019-05-26 16:25:54 +0200

    Updating main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_presentations/flutter_presentations.dart';
 import 'package:flutter_presentations/shared/animation_mode.dart';
 
 void main() {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  enableFlutterDriverExtension();
   runApp(
     const AnimationMode(
-      enabled: false, // Disables infinite animations for testing
+      enabled: true,
       child: const FlutterPresentations(),
     ),
   );
