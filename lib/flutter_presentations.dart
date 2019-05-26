3e6871bbc3c204653d310bab4e6655a271c2276e
Tomek Polanski <polanski.tomek@gmail.com>
2019-05-26 11:02:55 +0200

    Cleaning up main

---
+++ lib/flutter_presentations.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_presentations/features/code_reuse/true_code_reuse.dart';
+import 'package:flutter_presentations/features/convincing_for_flutter/convincing.dart';
+import 'package:flutter_presentations/features/slivers/slivers.dart';
+
+class FlutterPresentations extends StatelessWidget {
+  const FlutterPresentations({Key key}) : super(key: key);
+
+  static final GlobalKey gKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: gKey,
+      theme: ThemeData(fontFamily: 'Montserrat'),
+      home: const _PresentationList(),
+    );
+  }
+}
+
+class _PresentationList extends StatelessWidget {
+  const _PresentationList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Presentations'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(Slivers.title),
+            subtitle: Text(Slivers.subtitle),
+            onTap: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => Slivers())),
+          ),
+          ListTile(
+            title: Text(Convincing.title),
+            subtitle: Text(Convincing.subtitle),
+            onTap: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => Convincing())),
+          ),
+          ListTile(
+            title: Text(CodeReuse.title),
+            subtitle: Text(CodeReuse.subtitle),
+            onTap: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => CodeReuse())),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,65 +2,16 @@ import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter_presentations/features/code_reuse/true_code_reuse.dart';
-import 'package:flutter_presentations/features/convincing_for_flutter/convincing.dart';
-import 'package:flutter_presentations/features/slivers/slivers.dart';
+import 'package:flutter_presentations/flutter_presentations.dart';
 import 'package:flutter_presentations/shared/animation_mode.dart';
 
 void main() {
-  debugProfileBuildsEnabled = true;
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   enableFlutterDriverExtension();
-  runApp(AnimationMode(
-    enabled: false,
-    child: MyApp(),
-  ));
-}
-
-class MyApp extends StatelessWidget {
-  static final GlobalKey gKey = GlobalKey<NavigatorState>();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      navigatorKey: gKey,
-      theme: ThemeData(fontFamily: 'Montserrat'),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Presentations'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text(Slivers.title),
-            subtitle: Text(Slivers.subtitle),
-            onTap: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => Slivers())),
-          ),
-          ListTile(
-            title: Text(Convincing.title),
-            subtitle: Text(Convincing.subtitle),
-            onTap: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => Convincing())),
-          ),
-          ListTile(
-            title: Text(CodeReuse.title),
-            subtitle: Text(CodeReuse.subtitle),
-            onTap: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => CodeReuse())),
-          ),
-        ],
-      ),
-    );
-  }
+  runApp(
+    const AnimationMode(
+      enabled: false, // Disables infinite animations for testing
+      child: const FlutterPresentations(),
+    ),
+  );
 }
