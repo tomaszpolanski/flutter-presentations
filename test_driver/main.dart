6b97bdebf45908c42eb51cd830a693ff70740b5e
Tomek Polański <polanski.tomek@gmail.com>
2019-05-26 17:46:17 +0200

    Improved test driver (#6)
    * Improving test driver
    
    * Improving tests
    
    * Update main_test.dart

---
+++ test_driver/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter_presentations/flutter_presentations.dart';
+import 'package:flutter_presentations/shared/animation_mode.dart';
+
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  enableFlutterDriverExtension();
+  runApp(
+    const AnimationMode(
+      enabled: false,
+      child: const FlutterPresentations(),
+    ),
+  );
+}
