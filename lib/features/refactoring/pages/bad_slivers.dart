ba7862226ed3e78948369dc179ce42a4b79bc4de
Tomek Polanski <tpolanski@groupon.com>
2020-12-20 10:58:39 +0100

    Update bad_slivers.dart

--- lib/features/refactoring/pages/bad_slivers.dart
+++ lib/features/refactoring/pages/bad_slivers.dart
@@ -8,14 +8,52 @@ class BadSlivers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final items = List.generate(1000, (index) => index.toString());
     return Center(
       child: SizedBox(
         width: 400,
         height: 700,
         child: AppFrame(
-          title: Text('Bad'),
-          child: Placeholder(),
+          title: const Text('Bad'),
+          child: CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    for (final item in items)
+                      ListTile(
+                        title: Text(
+                          'Item number $item',
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomSliverList extends StatelessWidget {
+  const CustomSliverList(this.items, {Key? key}) : super(key: key);
+
+  final List<String> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => ListTile(
+          title: Text(
+            'Item number ${items[index]}',
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+        childCount: items.length,
       ),
     );
   }
