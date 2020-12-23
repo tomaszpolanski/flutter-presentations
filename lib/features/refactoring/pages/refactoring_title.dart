f47ce82bd70a05da13f60fa64288489d044243d3
Tomek Polanski <tpolanski@groupon.com>
2020-12-23 10:27:06 +0100

    wip

--- lib/features/refactoring/pages/refactoring_title.dart
+++ lib/features/refactoring/pages/refactoring_title.dart
@@ -1,56 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:presentation/presentation.dart';
 
-class RefactoringTitle extends StatelessWidget {
+class RefactoringTitle extends StatefulWidget {
   const RefactoringTitle({Key? key}) : super(key: key);
 
   @override
+  _RefactoringTitleState createState() => _RefactoringTitleState();
+}
+
+const text = 'Refactoring';
+
+Iterable<_Letter> arrange(String text) {
+  return text.split('').fold<List<_Letter>>(
+    [],
+    (previousValue, element) {
+      final letters = <_Letter>[];
+      if (previousValue.isEmpty) {
+        const index = 0;
+        letters.add(_Letter(
+          element,
+          horizontal: 400,
+          width: element.letter,
+          index: index,
+        ));
+      } else {
+        final prev = previousValue.last;
+        final index = prev.index + 1;
+        letters
+          ..addAll(previousValue)
+          ..add(_Letter(
+            element,
+            horizontal: prev.horizontal + prev.width,
+            width: element.letter,
+            index: index,
+          ));
+      }
+      return letters;
+    },
+  );
+}
+
+class _RefactoringTitleState extends State<RefactoringTitle>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  double _offset(int index) {
+    const e = 5;
+    final x = index - text.length / 2;
+    return (x < 0 ? -1 : 1) * e * x * x;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ...'Refactoring'.split('').fold<List<_Letter>>(
-          [],
-          (previousValue, element) {
-            final letters = <_Letter>[];
-            if (previousValue.isEmpty) {
-              letters.add(_Letter(element, offset: 400, width: element.letter));
-            } else {
-              letters
-                ..addAll(previousValue)
-                ..add(_Letter(
-                  element,
-                  offset: previousValue.last.offset + previousValue.last.width,
-                  width: element.letter,
-                ));
-            }
-            return letters;
-          },
-        ).toList(),
-      ],
+    return WrappedAnimatedBuilder<double>(
+      animation: CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+      builder: (context, animation, _) {
+        return Stack(
+          children: [
+            ...arrange(text).map((l) {
+              return Animation1(
+                l,
+                offset: _offset(l.index) * animation.value,
+              );
+            }),
+          ],
+        );
+      },
     );
   }
 }
 
-class _Letter extends StatelessWidget {
-  const _Letter(
-    this.letter, {
+class Animation1 extends StatelessWidget {
+  const Animation1(
+    this.l, {
     required this.offset,
-    required this.width,
     Key? key,
   }) : super(key: key);
+  final _Letter l;
 
-  final String letter;
   final double offset;
-  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: offset,
-      top: 300,
+      left: l.horizontal + offset,
+      top: 300 + l.vertical,
       child: Text(
-        letter,
+        l.letter,
         style: Theme.of(context).textTheme.headline4,
       ),
+    );
+  }
+}
+
+class _Letter {
+  const _Letter(
+    this.letter, {
+    required this.horizontal,
+    this.vertical = 0,
+    required this.width,
+    required this.index,
+  });
+
+  final String letter;
+  final double horizontal;
+  final double vertical;
+  final double width;
+  final int index;
+
+  _Letter copyWith({
+    double? horizontal,
+    double? width,
+    int? index,
+  }) {
+    return _Letter(
+      letter,
+      horizontal: horizontal ?? this.horizontal,
+      width: width ?? this.width,
+      index: index ?? this.index,
     );
   }
 }
