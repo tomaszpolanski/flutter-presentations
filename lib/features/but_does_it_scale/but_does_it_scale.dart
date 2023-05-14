734b260a312b30e6f990d9df4a58747e6713baec
Tomek Polański <tomek.polanski@klar.mx>
2023-05-14 11:15:57 +0200

    but does it scale initial page (#43)

---
+++ lib/features/but_does_it_scale/but_does_it_scale.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_presentations/features/code_reuse/pages/thats_all.dart';
+import 'package:flutter_presentations/shared/logo.dart';
+import 'package:flutter_presentations/shared/themes.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:presentation/presentation.dart';
+
+class ButDoesItScale extends StatefulWidget {
+  const ButDoesItScale({super.key});
+
+  static const String title = 'But does it scale?';
+  static const String subtitle = 'Yes!';
+
+  @override
+  _ButDoesItScaleState createState() => _ButDoesItScaleState();
+}
+
+class _ButDoesItScaleState extends State<ButDoesItScale> {
+  late PageController controller;
+  late PresentationController presentationController;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PageController();
+    presentationController = PresentationController(
+      controller: controller,
+      animationDuration: const Duration(milliseconds: 600),
+    );
+  }
+
+  @override
+  void dispose() {
+    presentationController.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: blueLight(),
+      child: Scaffold(
+        body: Builder(
+          builder: (_) {
+            final children = [
+              const ThatsAll(thanks: 'Thank you!'),
+              const ThatsAll(thanks: 'Thank you!'),
+            ];
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                Presentation(
+                  controller: controller,
+                  presentationController: presentationController,
+                  children: children,
+                ),
+                Positioned(
+                  bottom: 50,
+                  left: 50,
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (_, __) {
+                      final page = controller.page ?? 0.0;
+                      return Logo(
+                        visible: (page * 1000).floor() % 1000 == 0,
+                        child: Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/fluttercon.svg',
+                              height: 25,
+                              colorFilter: const ColorFilter.mode(
+                                Colors.blue,
+                                BlendMode.srcIn,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
