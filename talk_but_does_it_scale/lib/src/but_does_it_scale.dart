5ba984a6fc2446dd11571ec6f6e1ce6d67aae43a
Tomek Polański <tomek.polanski@klar.mx>
2023-06-03 11:24:55 +0200

    wip

---
+++ talk_but_does_it_scale/lib/src/but_does_it_scale.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:shared_theme/shared_theme.dart';
+import 'package:presentation/presentation.dart';
+import 'package:shared_pages/shared_pages.dart';
+import 'package:shared_ui/shared_ui.dart';
+
+class ButDoesItScale extends StatefulWidget {
+  const ButDoesItScale({super.key});
+
+  static const String title = 'But Does It Scale';
+  static const String subtitle =
+      'Can flutter applications scale to large teams?';
+
+  @override
+  _ButDoesItScaleState createState() => _ButDoesItScaleState();
+}
+
+class _ButDoesItScaleState extends State<ButDoesItScale> {
+  late PageController controller;
+  late PresentationController presentationController;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PageController();
+    presentationController = PresentationController(
+      controller: controller,
+      animationDuration: const Duration(milliseconds: 600),
+    );
+  }
+
+  @override
+  void dispose() {
+    presentationController.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: blueLight(),
+      child: Scaffold(
+        body: Builder(
+          builder: (_) {
+            final children = [
+              const ThatsAll(thanks: 'Thank you!'),
+            ];
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                Presentation(
+                  controller: controller,
+                  presentationController: presentationController,
+                  children: children,
+                ),
+                Positioned(
+                  bottom: 50,
+                  left: 50,
+                  child: PresentationLogo(
+                    controller: controller,
+                    child: const SizedBox(),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
