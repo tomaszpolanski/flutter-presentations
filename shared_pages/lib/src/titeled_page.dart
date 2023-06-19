1c3c99cda9238f2686a709c21e7f3b308d8c69df
Tomek Polański <polanski.tomek@gmail.com>
2023-06-19 08:06:23 +0200

    Old android project structure (#68)

---
+++ shared_pages/lib/src/titeled_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:presentation/effects.dart';
+
+class TiteledPage extends StatelessWidget {
+  const TiteledPage({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final Widget title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: ParallaxWidget(
+              translationFactor: 150,
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  color: Theme.of(context).textTheme.titleLarge?.color,
+                  height: 8,
+                  width: 100,
+                ),
+              ),
+            ),
+          ),
+          DefaultTextStyle.merge(
+            style: Theme.of(context).textTheme.titleLarge,
+            child: title,
+          ),
+          Expanded(child: child),
+        ],
+      ),
+    );
+  }
+}
---
+++ shared_ui/lib/src/project_structure.dart
@@ -0,0 +1,108 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:shared_ui/shared_ui.dart';
+
+class ProjectStructure extends StatefulWidget {
+  const ProjectStructure({
+    super.key,
+    this.expanded = false,
+    required this.modules,
+  });
+
+  final bool expanded;
+  final List<Module> modules;
+
+  @override
+  State<ProjectStructure> createState() => _ProjectStructureState();
+}
+
+class _ProjectStructureState extends State<ProjectStructure> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ...widget.modules.map(
+          (m) => _SingleModule(
+            m,
+            expanded: widget.expanded,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SingleModule extends StatefulWidget {
+  const _SingleModule(
+    this.module, {
+    required this.expanded,
+    // ignore: unused_element
+    super.key,
+  });
+
+  final Module module;
+  final bool expanded;
+
+  @override
+  State<_SingleModule> createState() => _SingleModuleState();
+}
+
+class _SingleModuleState extends State<_SingleModule> {
+  late bool _expanded = widget.expanded;
+
+  @override
+  Widget build(BuildContext context) {
+    final chevron = FadeInVisibility(
+      visible: widget.module.modules.isNotEmpty,
+      child: Transform.rotate(
+        angle: _expanded ? math.pi / 2 : 0,
+        child: const Icon(Icons.chevron_right_sharp),
+      ),
+    );
+
+    return GestureDetector(
+      onTap: widget.module.modules.isNotEmpty
+          ? () {
+              setState(() {
+                _expanded = !_expanded;
+              });
+            }
+          : null,
+      child: ColoredBox(
+        color: Colors.transparent,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            chevron,
+            FadeInVisibility(
+              visible: widget.module.type == Type.ui,
+              child: const FlutterLogo(),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.module.name,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  if (_expanded)
+                    ...widget.module.modules.map(
+                      (m) => _SingleModule(
+                        m,
+                        expanded: widget.expanded,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
