fdaa6668b4a77c6d7e05e406b771f97eb6115361
Tomek Polański <polanski.tomek@gmail.com>
2023-06-25 11:41:31 +0200

    Adding golden tests

---
+++ talk_but_does_it_scale/lib/src/assets.dart
@@ -0,0 +1,8 @@
+abstract class Assets {
+  static const package = 'talk_but_does_it_scale';
+
+  static const oldAndroid = 'packages/$package/assets/old_android.json';
+  static const newAndroid = 'packages/assets/new_android.json';
+  static const advanceGolden = 'assets/advance_golden.png';
+  static const simpleGolden = 'assets/simple_golden.png';
+}
---
+++ talk_but_does_it_scale/lib/src/pages/golden_tests.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:shared_ui/shared_ui.dart';
+import 'package:shared_pages/shared_pages.dart';
+
+class GoldenTestPage extends StatelessWidget {
+  const GoldenTestPage({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final Widget title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TiteledPage(
+      title: title,
+      // Should be only used for graphs
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 80),
+          child: Snippet(
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
