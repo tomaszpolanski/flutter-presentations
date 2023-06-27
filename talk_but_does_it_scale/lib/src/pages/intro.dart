a6a28e2b1ee47f4148f80bc88f116a75c7b4d91f
Tomek Polański <polanski.tomek@gmail.com>
2023-06-27 15:28:15 +0200

    Intro page part 1 (#77)

--- talk_but_does_it_scale/lib/src/pages/intro.dart
+++ talk_but_does_it_scale/lib/src/pages/intro.dart
@@ -6,6 +6,59 @@ class Intro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Orb();
+    return Row(
+      children: [
+        const Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 120, right: 30),
+                      child: Text('But', style: TextStyle(fontSize: 180)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50, right: 30),
+                      child: Text('does', style: TextStyle(fontSize: 90)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 120),
+                      child: Text('it', style: TextStyle(fontSize: 50)),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(),
+                    child: Text('Scale', style: TextStyle(fontSize: 180)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Transform.translate(
+                offset: const Offset(0, -70),
+                child: const Text('?', style: TextStyle(fontSize: 240)),
+              ),
+              Transform.scale(
+                scale: 1,
+                child: const Orb(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
