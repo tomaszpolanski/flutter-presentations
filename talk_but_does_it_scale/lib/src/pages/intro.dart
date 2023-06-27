57172bc6a12e950571c2d2e25a92e64342e45c83
Tomek Polański <polanski.tomek@gmail.com>
2023-06-27 19:11:43 +0200

    Improving intro (#79)

--- talk_but_does_it_scale/lib/src/pages/intro.dart
+++ talk_but_does_it_scale/lib/src/pages/intro.dart
@@ -9,7 +9,7 @@ class Intro extends StatelessWidget {
     return Row(
       children: [
         const Expanded(
-          flex: 2,
+          flex: 6,
           child: Column(
             children: [
               Expanded(
@@ -17,16 +17,16 @@ class Intro extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(bottom: 120, right: 30),
+                      padding: EdgeInsets.only(bottom: 120, right: 80),
                       child: Text('But', style: TextStyle(fontSize: 180)),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 50, right: 30),
-                      child: Text('does', style: TextStyle(fontSize: 90)),
+                      padding: EdgeInsets.only(top: 50, right: 80),
+                      child: Text('does', style: TextStyle(fontSize: 60)),
                     ),
                     Padding(
                       padding: EdgeInsets.only(top: 120),
-                      child: Text('it', style: TextStyle(fontSize: 50)),
+                      child: Text('it', style: TextStyle(fontSize: 20)),
                     ),
                   ],
                 ),
@@ -36,7 +36,7 @@ class Intro extends StatelessWidget {
                   alignment: Alignment.topRight,
                   child: Padding(
                     padding: EdgeInsets.only(),
-                    child: Text('Scale', style: TextStyle(fontSize: 180)),
+                    child: Text('scale', style: TextStyle(fontSize: 220)),
                   ),
                 ),
               ),
@@ -44,16 +44,17 @@ class Intro extends StatelessWidget {
           ),
         ),
         Expanded(
+          flex: 4,
           child: Stack(
             alignment: Alignment.center,
             children: [
               Transform.translate(
-                offset: const Offset(0, -110),
+                offset: const Offset(0, -170),
                 child: const ClipRect(
                   clipper: ClipperRect(Rect.fromLTRB(0, 0, 1, 0.6)),
                   child: Text(
                     '?',
-                    style: TextStyle(fontSize: 400),
+                    style: TextStyle(fontSize: 500),
                   ),
                 ),
               ),
